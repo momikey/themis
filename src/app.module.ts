b74a03b8cbe6b2a425cba411668dfbef3369f56b
Michael H. Potter <michael@potterpcs.net>
2018-11-21 20:51:24 -0500

    Added project skeleton, including dependencies

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir('views');
+  app.setViewEngine('ejs');
+
+  await app.listen(3000);
+}
+bootstrap();
