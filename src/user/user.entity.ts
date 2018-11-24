e8cfb3831b7fe032d3253660f84416099209a3c5
Michael H. Potter <michael@potterpcs.net>
2018-11-23 19:56:05 -0500

    Update to User schema

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -11,21 +11,26 @@ export class User {
     @Column('varchar', { length: 32 })
     name: string;
 
-    // `displayName` is the "preferred" name for the user.
+    // `server` is the name of the server the user is on.
+    // Mostly, this is for caching purposes.
     @Column('text')
+    server: string;
+
+    // `displayName` is the "preferred" name for the user.
+    @Column('text', { nullable: true })
     displayName: string;
 
     // `summary` is used for profile information, such as a bio or whatever.
-    @Column('text')
+    @Column('text', { nullable: true })
     summary: string;
 
     // `icon` is meant to contain a link (URI) to an icon image.
     // This is analagous to forum avatars or Usenet X-Face.
-    @Column('text')
+    @Column('text', { nullable: true })
     icon: string;
 
     // We store the date the user's record was created, mostly for debugging,
     // but also so we can do the "date joined" thing on profile pages.
-    @CreateDateColumn()
+    @CreateDateColumn({ readonly: true })
     date: string;
 }
