1cee40eb063d2e583551c643ac53cd2314b2be41
Michael H. Potter <michael@potterpcs.net>
2018-12-01 18:37:58 -0500

    Fixed base view dir

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir('views');
+  app.setBaseViewsDir(join(__dirname, '.', 'views'));
   app.setViewEngine('ejs');
 
   await app.listen(3000);
