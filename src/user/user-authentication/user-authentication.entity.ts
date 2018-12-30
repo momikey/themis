35a7b2aacb366d6940687b2b39bff75da5460123
Michael H. Potter <michael@potterpcs.net>
2018-12-30 02:29:18 -0500

    Fixed dumb mistake where I forgot to make the columns columns

--- src/user/user-authentication/user-authentication.entity.ts
+++ src/user/user-authentication/user-authentication.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, Column } from "typeorm";
 import { User } from "../user.entity";
 
 @Entity()
@@ -16,18 +16,22 @@ export class UserAuthentication {
 
     // The user's email address, as given during signup.
     // (Worry about all the GDPR stuff later...)
+    @Column('text')
     email: string;
 
     // The user's password. Obviously, this needs to be salted, hashed,
     // and baked into a delicious meal. Or however that works.
+    @Column('text')
     password: string;
 
     // A simple flag to indicate whether a user has requested a password reset.
     // We don't do anything with that just yet, but give it time.
+    @Column('boolean')
     reset: boolean;
 
     // JWT bearer token. I'm not sure if we're supposed to store this,
     // but it makes sense. After all, we'll be storing it on the *user* side.
+    @Column('text', {nullable: true})
     token: string;
 
     // TODO: Do we need a date field here? Might be useful for password expiration.
