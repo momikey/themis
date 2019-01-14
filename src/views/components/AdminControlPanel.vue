652606a4e0301c841ed060e8c9e420f114b09de3
Michael H. Potter <michael@potterpcs.net>
2019-01-13 23:19:02 -0500

    Nav drawer for admin panel

--- src/views/components/AdminControlPanel.vue
+++ src/views/components/AdminControlPanel.vue
@@ -1,12 +1,70 @@
 <template>
-    
+    <v-container fluid>
+        <v-layout justify-start row fill-height wrap>
+            <v-flex xs3>
+            <!-- Navigation -->
+            <v-navigation-drawer app dark permanent>
+                <v-toolbar>
+                    <v-list>
+                        <v-list-tile>
+                        <v-list-tile-title class="title">
+                            Themis Admin Panel
+                        </v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-toolbar>
+
+                <v-divider></v-divider>
+
+                <v-list>
+                    <v-list-tile
+                        v-for="item in panelItems"
+                        :key="item.title"
+                        @click="navigate(item.action)"
+                    >
+                        <v-list-tile-action>
+                            <v-icon>{{item.icon}}</v-icon>
+                        </v-list-tile-action>
+
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-navigation-drawer>
+            </v-flex>
+
+            <v-flex xs9>
+            <!-- Body components -->
+            <component :is="currentComponent" />
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script lang="ts">
 import Vue, { VueConstructor } from 'vue'
 
 export default Vue.extend({
-    
+    data () {
+        return {
+            currentComponent: null,
+
+            panelItems: [
+                { title: 'Users', icon: 'people', action: 'users' },
+                { title: 'Groups', icon: 'category', action: 'groups' },
+                { title: 'Site Preferences', icon: 'settings', action: 'prefs' },
+                { title: 'Log out', icon: 'power_settings_new', action: 'logout' }
+            ]
+        }
+    },
+    methods: {
+        navigate (loc) {
+            // TODO
+            console.log(loc);
+            
+        },
+    }
 })
 
 </script>
