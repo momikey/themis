318668f3b6d5efe6923789ad4748b68859b6329f
Michael H. Potter <michael@potterpcs.net>
2019-04-25 19:17:51 -0400

    Set up body-parser to use proper AP Content-Type values

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import { json } from 'body-parser';
+import { AP } from './activitypub/definitions/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // By default, body-parser (which Nest uses via Express),
+  // only reads bodies of POST requests with Content-Type
+  // 'application/json'. AP requires others, so we have to
+  // tell it to handle those, too.
+  app.use(json({ type: (req) => {
+    return req.headers['content-type'].startsWith('application/json') ||
+      req.headers['content-type'] === AP.ContentType ||
+      req.headers['content-type'] === AP.AltContentType
+  }}));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '.', 'frontend'));
